199fcf1f6209e6ba27d200a6416f6f0fa23e1378
elder-tome <r14.elderalves@gmail.com>
2020-11-10 23:32:31 -0300

    create Cast component

--- src/pages/Tv.tsx
+++ src/pages/Tv.tsx
@@ -2,11 +2,24 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+import Cast from '../components/Cast';
 import api from '../services/api';
 import './Tv.css';
 
 interface params {
   id: string
+}
+
+// interface cast {
+//   id: number,
+//   name: string,
+//   profile_path: string,
+//   order: number
+// }
+interface video {
+  id: number,
+  name: string,
+  key: string
 }
 
 function Tv() {
@@ -23,20 +36,15 @@ function Tv() {
     overview: '',
     homepage: '',
     first_air_date: '',
-    last_air_date: '',
     episode_run_time: [0],
     in_production: true,
     number_of_seasons: 0,
     number_of_episodes: 0,
-    networks: [
-      {
-        id: 0,
-        name: '',
-        logo_path: '',
-      }
-    ],
     vote_average: 0.0
   });
+
+  // const [cast, setCast] = useState<cast[]>([]);
+  const [video, setVideo] = useState<video[]>([]);
 
   const { id } = useParams<params>();
 
@@ -52,8 +60,37 @@ function Tv() {
           language: 'pt-BR'
         }
       });
+      // const responseCast = await api.get(`/tv/${id}/credits`, {
+      //   params: {
+      //     api_key: process.env.REACT_APP_TOKEN,
+      //     language: 'pt-BR'
+      //   }
+      // });
+
+      let responseVideo = await api.get(`/tv/${id}/videos`, {
+        params: {
+          api_key: process.env.REACT_APP_TOKEN,
+          language: 'pt-BR'
+        }
+      });
+
+      if (responseVideo.data.results.length <= 0) {
+        responseVideo = await api.get(`/tv/${id}/videos`, {
+          params: {
+            api_key: process.env.REACT_APP_TOKEN,
+            language: 'en-US'
+          }
+        });
+
+        setVideo(responseVideo.data.results);
+
+      }
+      else {
+        setVideo(responseVideo.data.results);
+      }
 
       setTv(response.data);
+      // setCast(responseCast.data.cast);
 
     })();
 
@@ -66,23 +103,26 @@ function Tv() {
       <img src={`https://image.tmdb.org/t/p/w500${tv.backdrop_path}`} alt="" />
       <div className='main-container'>
         <h1>{tv.name}</h1>
-        <p className='overview'>{tv.overview}</p>
+        <p>{tv.first_air_date}</p>
+        <p>{tv.in_production ? 'Em Produção' : 'Produção finalizada'}</p>
+        <p>{`Nota ${tv.vote_average}`}</p>
         {tv.genres.map(genres => (
           <p key={genres.id}>{genres.name}</p>
         ))}
+        <p className='overview'>{tv.overview}</p>
         <p>{`${tv.number_of_seasons} Temporada(s)`}</p>
         <p>{`${tv.number_of_episodes} Episódeos`}</p>
         <p>{`${tv.episode_run_time} minutos`}</p>
-        {/* <p>{tv.first_air_date}</p>
-        <p>{tv.last_air_date}</p> */}
-        <p>{tv.in_production ? 'Em Produção' : 'Produção finalizada'}</p>
-        <p>{`Nota ${tv.vote_average}`}</p>
-        {/* <p>{tv.homepage}</p> */}
-      </div>
-      <div className='network-container'>
-        {tv.networks.map(networks => (
-          <img key={networks.id} src={`https://image.tmdb.org/t/p/w500${networks.logo_path}`} alt="" />
+        <Cast  media='tv' id={id}/>
+        {video.map(video => (
+          <div className='video-container'>
+            <div className='videos' key={video.id}>
+              <p>{video.name}</p>
+              <p>{`https://www.youtube.com/watch?v=${video.key}`}</p>
+            </div>
+          </div>
         ))}
+        <p>{tv.homepage}</p>
       </div>
     </div>
   );
