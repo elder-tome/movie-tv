fb8891942bf3924b219f34e9960542d2b429345a
elder-tome <r14.elderalves@gmail.com>
2021-05-27 07:04:59 -0300

    creation of tv screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,11 @@
 import Head from '../components/Head';
-// import { slide as Menu } from 'react-burger-menu';
 
-// import styles from '../styles/tv.module.css';
-
-export default function Tv() {
+export default function Home() {
   return (
     <div className='container'>
       <Head />
       <header>
-        {/* <Menu
-          className='menu'
-          customBurgerIcon={<img src={mdi_menu} alt="" />}
-          customCrossIcon={false}
-          width={250}
-        > */}
-        <h1>Tv</h1>
+        <h1>Inicial</h1>
       </header>
       <main></main>
     </div>
